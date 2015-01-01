a15c39186038e38b062cef6bd97cf4777146c08e
Mandar <01.mandar@gmail.com>
2015-01-01 15:45:11 +0530

    weather check android application

---
+++ app/src/main/java/com/ms/localweather/Reporter.java
@@ -0,0 +1,139 @@
+package com.ms.localweather;
+
+/**
+ * Created by mandar on 01-Jan-15.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Reporter implements LocationListener {
+    private static final String OPEN_WEATHER_MAP_API =
+            "http://api.openweathermap.org/data/2.5/weather?";//"http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    static boolean isGPSEnabled = false;
+    static boolean isNetworkEnabled = false;
+    static Location location = null;
+    static float latitude;
+    static float longitude;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    protected LocationManager locationManager;
+    private Location m_Location;
+
+    public static JSONObject getJSON(Context context, String city){
+        try {
+
+            //Declaring a Location Manager
+            LocationManager locationManager;
+
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+            //getting GPS status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            //getting network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+
+            StringBuilder useurl = new StringBuilder();
+
+            if (!isGPSEnabled && !isNetworkEnabled)
+            {
+                useurl.append(OPEN_WEATHER_MAP_API).append("q=").append(city).append("&units=metric");
+            }
+            else
+            {
+
+                if (isNetworkEnabled) {
+
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = (float)  location.getLatitude();
+                            longitude= (float)location.getLongitude();
+                        }
+                    }
+                }
+                if (isGPSEnabled) {
+                    if (location == null) {
+
+                        Log.d("GPS", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude =(float)  location.getLatitude();
+                                longitude=(float) location.getLongitude();
+                            }
+                        }
+                    }
+                }
+                useurl.append(OPEN_WEATHER_MAP_API).append("lat=").append(String.valueOf(latitude)).append("&lon=").append(String.valueOf(longitude)).append("&units=metric");
+            }
+
+
+
+            URL url = new URL(useurl.toString());
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+
+            connection.addRequestProperty("x-api-key","e0bb0993b225c240f2ce1380e93bb19c");
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not
+            // successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
---
+++ app/src/main/java/com/ms/localweather/WeatherMainActivity.java
@@ -0,0 +1,56 @@
+package com.ms.localweather;
+
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+
+
+public class WeatherMainActivity extends ActionBarActivity {
+
+    Dashboard dashboard;
+    FrameLayout frameContainer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_main);
+
+        frameContainer = (FrameLayout)findViewById(R.id.container);
+        dashboard= new Dashboard();
+
+        if (savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container,dashboard)
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+ //       getMenuInflater().inflate(R.menu.menu_weather_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
